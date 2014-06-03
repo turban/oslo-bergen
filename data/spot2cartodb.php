ff60b2ed742f988218c6a278154e2dc3c3087a10
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2014-06-03 21:30:54 +0200

    Credentials

---
+++ data/spot2cartodb.php
@@ -0,0 +1,76 @@
+<?php
+
+date_default_timezone_set('Europe/Oslo');
+
+require_once 'cartodb.class.php';
+
+$feedId = "123abc";
+$feedUrl = "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/$feedId/message.json";
+
+$jsonString = file_get_contents($feedUrl);
+$jsonArr    = json_decode($jsonString, true);
+$response   = $jsonArr['response'];
+
+if (isset($response['errors'])) {
+  $error = $response['errors']['error'];
+  error_log($error['code'] . $error['text']);
+  print $error['description'];
+  exit();  
+}
+
+$response = $jsonArr['response']['feedMessageResponse'];
+
+if ($response['count'] == 1) {
+  $messages = array($response['messages']['message']);
+} else {
+  $messages = $response['messages']['message'];
+}
+
+// CartoDB
+$config = array(
+  "key"       => "123abc",
+  "secret"    => "123abc",
+  "email"     => "your@email",
+  "password"  => "password",
+  "subdomain" => "account"
+);
+
+$cartodb = new CartoDBClient($config);
+
+if (!$cartodb->authorized) {
+  error_log("uauth");
+  print 'There is a problem authenticating, check the key and secret.';
+  exit();
+}
+
+// Find max timestamp
+$response = $cartodb->runSql("SELECT MAX(timestamp) FROM spot");
+$max = array_pop($response['return']['rows'])->max;
+
+foreach ($messages as $message) {
+
+  if ($message['unixTime'] > $max) { // Add if newer
+    $data = array(
+      'feed_id'         => "'" . $feedId . "'",
+      'spot_id'         => $message['id'],
+      'the_geom'        => "'SRID=4326;POINT(" . $message['longitude'] . " " . $message['latitude'] . ")'",
+      'messenger_id'    => "'" . $message['messengerId'] . "'",
+      'messenger_name'  => "'" . $message['messengerName'] . "'",
+      'timestamp'        => $message['unixTime'],    
+      'message_type'    => "'" . $message['messageType'] . "'",  
+      'latitude'        => $message['latitude'],  
+      'longitude'       => $message['longitude'],
+      'model_id'        => "'" . $message['modelId'] . "'",  
+      'show_custom_msg' => "'" . $message['showCustomMsg'] . "'",
+      'datetime'        => "'" . date("Y-m-d H:i:s", $message['unixTime']) . "'",
+      'battery_state'   => "'" . $message['batteryState'] . "'",
+      'hidden'          => "'" . $message['hidden'] . "'",
+      'message_content' => "'" . (isset($message['messageContent']) ? $message['messageContent'] : '') . "'"      
+    );
+
+    $response = $cartodb->insertRow('spot', $data);
+  }
+
+}
+
+?>
