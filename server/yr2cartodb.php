31d3af955da44e228abe4ae127c044aaab9bc00d
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2014-06-11 15:01:13 +0200

    yr.no fix

--- server/yr2cartodb.php
+++ server/yr2cartodb.php
@@ -1,10 +1,5 @@
 <?php
 
-// http://api.yr.no/weatherapi/locationforecast/1.9/?lat=59.33896;lon=5.96669
-// http://api.yr.no/weatherapi/locationforecast/1.9/documentation
-// http://nrkbeta.no/2014/06/03/bedre-nedborsvarsling-pa-yr/
-// http://om.yr.no/forklaring/symbol/
-
 require_once 'cartodb.class.php';
 require_once 'config.php';
 
@@ -19,11 +14,7 @@ if (!$cartodb->authorized) {
 }
 
 $lastHour = time() - 3600;
-//$lastHour = time() - 3600 * 24;
-$response = $cartodb->runSql("SELECT cartodb_id, timestamp, latitude, longitude FROM spot WHERE weather_symbol IS NULL AND timestamp > $lastHour ORDER BY timestamp DESC "); 
-//$response = $cartodb->runSql("SELECT cartodb_id, timestamp, latitude, longitude FROM spot WHERE weather_symbol IS NULL AND timestamp > $lastHour ORDER BY timestamp DESC LIMIT 1");
-
-//xprint_r($response);
+$response = $cartodb->runSql("SELECT cartodb_id, timestamp, latitude, longitude FROM spot WHERE weather_symbol IS NULL AND timestamp > $lastHour ORDER BY timestamp DESC"); 
 
 foreach ($response['return']['rows'] as $row) {
 	$cartodb_id = $row->cartodb_id;
@@ -33,11 +24,8 @@ foreach ($response['return']['rows'] as $row) {
 	$url        = "http://api.yr.no/weatherapi/locationforecast/1.9/?lat=$latitude;lon=$longitude";
 	$xml        = simplexml_load_file($url);
 	$time       = date("Y-m-d\TH:i:s\Z", round($timestamp / 3600) * 3600); // Nearest hour
-	$hour       = (int)date("G", $timestamp);
 	$symbolTime = date("Y-m-d\TH:00:00\Z", $timestamp); // This hour
-
-	//$location   = $xml->xpath("/weatherdata/product/time[@from='$time']/location")[0];
-	$location   = $xml->xpath("/weatherdata/product/time[@from='$time']/location");
+	$location   = $xml->xpath("/weatherdata/product/time[@from='$time']/location")[0];
 
 	if (count($location)) {
 		$location      = $location[0];
@@ -45,36 +33,23 @@ foreach ($response['return']['rows'] as $row) {
 		$windDirection = (string)$location->windDirection['name'];
 		$windSpeed     = (int)$location->windSpeed['mps'];
 		$wind          = (string)$location->windSpeed['name'];
-		//$cloudiness  = (int)$location ->cloudiness['percent'];
-		//$fog         = (int)$location ->fog['percent'];
 		$forecast      = $xml->xpath("/weatherdata/product/time[@from='$symbolTime'][2]")[0];
 		$symbolTime    = array('from' => (string)$forecast['from'], 'to' => (string)$forecast['to']);
 		$symbol        = (int)$forecast->location->symbol['number']; 
 		$precipitation = $forecast->location->precipitation['value']; 
-
 		$time          = new DateTIME('now', new DateTimeZone($timezone));
 		$sunrise       = strtotime(date_sunrise($time->getTimestamp(), SUNFUNCS_RET_STRING, $latitude , $longitude, 90, $time->getOffset() / 3600));
 		$sunset        = strtotime(date_sunset($time->getTimestamp(), SUNFUNCS_RET_STRING, $latitude , $longitude, 90, $time->getOffset() / 3600));
 
-		if ($symbol < 9 && $symbol !== 4) $symbol .= ($timestamp > $sunrise and $timestamp < $sunset) ? 'd' : 'n';
-		if (strlen($symbol) < 9) $symbol = "0$symbol";
-
-		/*
-		echo '<p>Time: ' . date("Y-m-d\TH:i:s\Z", $timestamp);
-		echo '<p>Nearest: ' . $time;
-		echo '<p>Temperature: ' . $temperature;
-		echo '<p>Wind: ' . $wind;
-		echo '<p>Wind speed: ' . $windSpeed;
-		echo '<p>Wind direction: ' . $windDirection;
-		echo '<p>Precipitation: ' . $precipitation;
-		echo "<p>Hour: $hour</p>";
-		echo "<p>Symbol: $symbol</p>";
-		echo '<img src="http://fil.nrk.no/yr/grafikk/sym/b38/' . $symbol . '.png" width="38" height="38" alt="Klårvêr">';
-		*/
+		if (strlen($symbol) < 2) { 
+			$symbol = "0$symbol";
+		}
+		if ($symbol < 9 && $symbol !== 4) {
+			$symbol .= ($timestamp > $sunrise and $timestamp < $sunset) ? 'd' : 'n';
+		}
 
 		$cartodb->runSql("UPDATE spot SET temperature=$temperature, precipitation=$precipitation, wind='$wind', wind_speed=$windSpeed, wind_direction='$windDirection', weather_symbol='$symbol' WHERE cartodb_id = $cartodb_id");
 	}
-
 }
 
 ?>
